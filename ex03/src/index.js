33f1122353afd4d54bb4bd5bdfc6f440c5d0a359
zivojinminic <zivojinminic05@gmail.com>
2021-09-17 11:22:45 +0200

    Escape sequences in string

---
+++ ex03/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    var myString = 'FirstLine\n\t\\SecondLine\nThirdLine'
+    // Only change code above this line
+    return myString;
+}
+
+console.log(main());
+module.exports = main;

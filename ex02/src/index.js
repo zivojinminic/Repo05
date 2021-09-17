39b1f11b1961dd237b13740e0dee7900c48d6169
zivojinminic <zivojinminic05@gmail.com>
2021-09-17 14:02:25 +0200

    Quoting strings

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line
-    var myLink = '<a href="http://www.example.com" target="_blank">Link</a>'
+    var myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
     // Only change code above this line
     return myLink;
 }
